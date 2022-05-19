316498d322c353b32beee98bdce83afe7ca3edbd
Shuang-Shuang Chen <shuangshuang0411@gmail.com>
2022-05-19 21:52:12 +0800

    Add first 1D hydro code 1D_Hydro_HLLC_evol.c and 1D_evol.py

---
+++ 1D_Hydro_HLLC_evol.c
@@ -0,0 +1,253 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+float Gamma = 5.0/3.0;
+int N = 1000;
+double T = 1.0;
+
+
+// primitive variables to conserved variables
+// E = P/(gamma-1) + 0.5*tho*v^2
+void Conserved2Primitive ( double (*U)[N], double (*pri)[N] ){
+    
+    for (int i=0;i<N;i++){
+        pri[0][i]=U[0][i];
+        pri[1][i]=U[1][i]/U[0][i];
+        pri[2][i]=(Gamma-1.0)*(U[2][i]-0.5*pow(U[1][i],2)/U[0][i]);
+    }
+}
+
+// conserved variables to primitive variables
+void Primitive2Conserved ( double (*pri)[N], double (*cons)[N] ){
+    
+    for (int i=0;i<N;i++){
+        cons[0][i]=pri[0][i];
+        cons[1][i]=pri[1][i]*pri[0][i];
+        cons[2][i]=pri[2][i]/(Gamma-1)+0.5*pri[0][i]*pow(pri[1][i],2);
+    }
+}
+
+double ComputePressure( double tho, double px, double e ){
+    double p = (Gamma-1.0)*( e - 0.5*pow(px,2)/tho);
+    return p;
+}
+
+void SoundSpeedMax( double (*U)[N], double *s_max) {
+    double s[N], p[N];
+    for (int i=0;i<N;i++){
+        p[i] = ComputePressure(U[0][i],U[1][i],U[2][i]);
+        s[i] = sqrt(Gamma*p[i]/U[0][i]);
+        if (U[1][i]/U[0][i]>=0)  s[i] += U[1][i]/U[0][i];
+        else  s[i] -= U[1][i]/U[0][i];
+    }
+    *s_max = 0.;
+    for (int i=0;i<N;i++){
+        if (s[i]>*s_max)  *s_max = s[i];
+    }
+}
+
+void Conserved2Flux ( double (*U)[N], double (*flux)[N] ){
+    
+    for (int i=0;i<N;i++){
+        double P = ComputePressure( U[0][i], U[1][i], U[2][i]);
+        double u = U[1][i] / U[0][i];
+        flux[0][i] = U[1][i];
+        flux[1][i] = u*U[1][i] + P;
+        flux[2][i] = (P+U[2][i])*u;
+    }
+}
+
+void HLLC_Riemann_Solver ( double (*U_L)[N], double (*U_R)[N], double(*HLLC_flux)[N] ){
+    
+    double (*F_L)[N] = (double(*)[N])malloc(3*sizeof(*F_L));
+    double (*F_R)[N] = (double(*)[N])malloc(3*sizeof(*F_R));
+    
+    double (*F_star_L)[N] = (double(*)[N])malloc(3*sizeof(*F_star_L));
+    double (*F_star_R)[N] = (double(*)[N])malloc(3*sizeof(*F_star_R));
+    
+    double a_L[N], a_R[N];
+    double u_L[N], u_R[N];
+    double p_R[N], p_L[N], p_star[N];
+    double q_L[N], q_R[N], S_L[N], S_R[N], S_star[N];
+    
+    for (int i=0;i<N;i++){
+        
+        u_L[i] = U_L[1][i]/U_L[0][i];
+        u_R[i] = U_R[1][i]/U_R[0][i];
+        p_L[i] = ComputePressure(U_L[0][i],U_L[1][i],U_L[2][i]);
+        p_R[i] = ComputePressure(U_R[0][i],U_R[1][i],U_R[2][i]);
+        a_L[i] = sqrt(Gamma*p_L[i]/U_L[0][i]);
+        a_R[i] = sqrt(Gamma*p_R[i]/U_R[0][i]);
+        
+        //step 1: pressure estimate
+        p_star[i] = 0.5*(p_L[i]+p_R[i])-0.5*(u_R[i]-u_L[i])*0.5*(U_L[0][i]+U_R[0][i])*0.5*(a_L[i]+a_R[i]);
+        if (p_star[i]<0)   p_star[i] = 0.;
+        
+        //step 2: wave speed estimate
+        if (p_star[i]>p_L[i]){
+            q_L[i] = sqrt(1+(Gamma+1)*(p_star[i]/p_L[i]-1)/2.0/Gamma);
+        }
+        else  q_L[i]=1.0;
+        if (p_star[i]>p_R[i]){
+            q_R[i] = sqrt(1+(Gamma+1)*(p_star[i]/p_R[i]-1)/2.0/Gamma);
+        }
+        else  q_R[i]=1.0;
+        
+        S_L[i] = u_L[i]-a_L[i]*q_L[i];
+        S_R[i] = u_R[i]+a_R[i]*q_R[i];
+        S_star[i] = (p_R[i]-p_L[i]+U_L[1][i]*(S_L[i]-u_L[i])-U_R[1][i]*(S_R[i]-u_R[i]))/(U_L[0][i]*(S_L[i]-u_L[i])-U_R[0][i]*(S_R[i]-u_R[i]));
+        
+        //step 3: HLLC flux
+        Conserved2Flux(U_L, F_L);
+        Conserved2Flux(U_R, F_R);
+        
+        F_star_L[0][i] = S_star[i]*(S_L[i]*U_L[0][i]-F_L[0][i])/(S_L[i]-S_star[i]);
+        F_star_L[1][i] = (S_star[i]*(S_L[i]*U_L[1][i]-F_L[1][i])+S_L[i]*(p_L[i]+U_L[0][i]*(S_L[i]-u_L[i])*(S_star[i]-u_L[i])))/(S_L[i]-S_star[i]);
+        F_star_L[2][i] = (S_star[i]*(S_L[i]*U_L[2][i]-F_L[2][i])+S_L[i]*S_star[i]*(p_L[i]+U_L[0][i]*(S_L[i]-u_L[i])*(S_star[i]-u_L[i])))/(S_L[i]-S_star[i]);
+        F_star_R[0][i] = S_star[i]*(S_R[i]*U_R[0][i]-F_R[0][i])/(S_R[i]-S_star[i]);
+        F_star_R[1][i] = (S_star[i]*(S_R[i]*U_R[1][i]-F_R[1][i])+S_R[i]*(p_R[i]+U_L[0][i]*(S_R[i]-u_R[i])*(S_star[i]-u_R[i])))/(S_R[i]-S_star[i]);
+        F_star_R[2][i] = (S_star[i]*(S_R[i]*U_R[2][i]-F_R[2][i])+S_R[i]*S_star[i]*(p_R[i]+U_L[0][i]*(S_R[i]-u_R[i])*(S_star[i]-u_R[i])))/(S_R[i]-S_star[i]);
+        
+        if (S_L[i]>=0){
+            HLLC_flux[0][i] = F_L[0][i];
+            HLLC_flux[1][i] = F_L[1][i];
+            HLLC_flux[2][i] = F_L[2][i];
+        }
+        else if (S_L[i]<=0 && S_star[i]>=0){
+            HLLC_flux[0][i] = F_star_L[0][i];
+            HLLC_flux[1][i] = F_star_L[1][i];
+            HLLC_flux[2][i] = F_star_L[2][i];
+        }
+        else if (S_star[i]<=0 && S_R[i]>=0){
+            HLLC_flux[0][i] = F_star_R[0][i];
+            HLLC_flux[1][i] = F_star_R[1][i];
+            HLLC_flux[2][i] = F_star_R[2][i];
+        }
+        else if (S_R[i]<=0){
+            HLLC_flux[0][i] = F_R[0][i];
+            HLLC_flux[1][i] = F_R[1][i];
+            HLLC_flux[2][i] = F_R[2][i];
+        }
+    }
+    free(F_L);
+    free(F_R);
+    free(F_star_L);
+    free(F_star_R);
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    double (*U)[N] = (double(*)[N])malloc(3*sizeof(*U));
+    double (*W)[N] = (double(*)[N])malloc(3*sizeof(*W));
+    double (*HLLC_flux_L)[N] = (double(*)[N])malloc(3*sizeof(*HLLC_flux_L));
+    double (*HLLC_flux_R)[N] = (double(*)[N])malloc(3*sizeof(*HLLC_flux_R));
+    double (*U_L)[N] = (double(*)[N])malloc(3*sizeof(*U_L));
+    double (*U_R)[N] = (double(*)[N])malloc(3*sizeof(*U_R));
+    
+    double dx = 1.0/128.0;
+    double dt;
+    double t = 0.;
+    double S_max = 6.29;
+    
+    //save data into file
+    FILE * data_ptr;
+    data_ptr = fopen("/Users/mac/Desktop/data_evol.txt", "w");
+    if (data_ptr==0)  return 0;
+    
+    //set the initial condition
+    for (int i=0;i<N;i++){
+        if (i<N/2-1){
+            W[0][i] = 1.0;
+            W[1][i] = 0.0;
+            W[2][i] = 1.0;
+        }
+        else{
+            W[0][i] = 0.125;
+            W[1][i] = 0.0;
+            W[2][i] = 0.1;
+        }
+    }
+    
+    for (int i=0;i<3;i++){
+        for (int j=0;j<N;j++){
+            fprintf(data_ptr,"%e ", W[i][j]);
+        }
+        fprintf(data_ptr,"\n");
+    }
+    
+    Primitive2Conserved(W, U);
+    
+    while (t<=T){
+        
+        for (int i=0;i<N-1;i++){
+            U_R[0][i] = U[0][i+1];
+            U_R[1][i] = U[1][i+1];
+            U_R[2][i] = U[2][i+1];
+        }
+        U_R[0][N-1] = U[0][N-1];
+        U_R[1][N-1] = U[1][N-1];
+        U_R[2][N-1] = U[2][N-1];
+        for (int i=1;i<N;i++){
+            U_L[0][i] = U[0][i-1];
+            U_L[1][i] = U[1][i-1];
+            U_L[2][i] = U[2][i-1];
+        }
+        U_L[0][0] = U[0][0];
+        U_L[1][0] = U[1][0];
+        U_L[2][0] = U[2][0];
+        
+        //compute dt
+        SoundSpeedMax(U, &S_max);
+        //printf("%.2f ",S_max);  //debug
+        dt = dx/S_max;
+        t += dt;
+        
+        //update data
+        HLLC_Riemann_Solver(U,U_R,HLLC_flux_R);
+        HLLC_Riemann_Solver(U_L,U,HLLC_flux_L);
+        for (int i=1;i<N-1;i++){
+            U[0][i] -= (HLLC_flux_R[0][i]-HLLC_flux_L[0][i])*dt/dx;
+            U[1][i] -= (HLLC_flux_R[1][i]-HLLC_flux_L[1][i])*dt/dx;
+            U[2][i] -= (HLLC_flux_R[2][i]-HLLC_flux_L[2][i])*dt/dx;
+        }
+        //boundary condition: outflow
+        U[0][0] = U[0][1];
+        U[1][0] = U[1][1];
+        U[2][0] = U[2][1];
+        U[0][N-1] = U[0][N-2];
+        U[1][N-1] = U[1][N-2];
+        U[2][N-1] = U[2][N-2];
+    }
+    
+    Conserved2Primitive(U, W);
+    
+    for (int i=0;i<3;i++){
+        for (int j=0;j<N;j++){
+            printf("%e ", W[i][j]);
+        }
+        printf("\n\n");
+    }
+    
+    //save data into file
+    for (int i=0;i<3;i++){
+        for (int j=0;j<N;j++){
+            fprintf(data_ptr,"%e ", W[i][j]);
+        }
+        fprintf(data_ptr,"\n");
+    }
+
+    fclose(data_ptr);
+    
+    free(U);
+    free(HLLC_flux_L);
+    free(HLLC_flux_R);
+    free(U_R);
+    free(U_L);
+    free(W);
+    
+    return 0;
+}
+
---
+++ 1D_evol.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+num = np.arange(1000)
+
+data = np.loadtxt("/Users/mac/Desktop/data_evol.txt")
+
+W0_orig = data[0]
+W1_orig = data[1]
+W2_orig = data[2]
+W0_evol = data[3]
+W1_evol = data[4]
+W2_evol = data[5]
+
+s_speed = (5.0/3.0*data[5][370]/data[3][370])**0.5
+print(s_speed,data[4][370],data[4][370]/s_speed)
+
+evol_fig = plt.figure()            
+
+plt.subplot(3,2,1)
+plt.plot(num, W0_orig, 'o', color='red') 
+plt.ylabel('density')
+plt.title('Initial Condition')
+plt.subplot(3,2,3)
+plt.plot(num, W1_orig, 'o', color='green') 
+plt.ylabel('velocity')
+plt.subplot(3,2,5)
+plt.plot(num, W2_orig, 'o', color='blue') 
+plt.xlabel('position')
+plt.ylabel('pressure')
+
+plt.subplot(3,2,2)
+plt.plot(num, W0_evol, 'o', color='red') 
+plt.ylabel('density')
+plt.title('Evolution')
+plt.subplot(3,2,4)
+plt.plot(num, W1_evol, 'o', color='green') 
+plt.ylabel('velocity')
+plt.subplot(3,2,6)
+plt.plot(num, W2_evol, 'o', color='blue') 
+plt.xlabel('position')
+plt.ylabel('pressure')
+
+plt.suptitle('Evolution of 1D Hydro tube')
+
+plt.show()                   
+
